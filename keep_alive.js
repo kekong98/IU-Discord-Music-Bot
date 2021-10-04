b7084cf93f034f1e18ca73293a38d5c4c95494e5
kekong98 <88658027+kekong98@users.noreply.github.com>
2021-10-04 19:38:29 +0900

    24/7 Update

--- keep_alive.js
+++ keep_alive.js
@@ -3,4 +3,4 @@ const app = express();
 const port = 2323;
 app.get('/', (req. res) => res.send('IU-Discord-Music-Bot'));
 
-app.listen(port, () => console.log('IU-Discord-Music-Bot is connected to http://localhost:${port}`));
+app.listen(port, () => console.log('Connected to http://localhost:${port}`));
